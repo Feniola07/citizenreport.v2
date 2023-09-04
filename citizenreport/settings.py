c71463aeff53ab76be047fd501c410f212e23583
Dev <devkreat@famcorpp.com>
2023-09-04 16:24:23 +0200

    staticfiles update

--- citizenreport/settings.py
+++ citizenreport/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9o%cez^vef0%y39)e41b*-=sipqx&j3wrtg%5gt-t4vjsu=f2l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,13 +142,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  = BASE_DIR / 'staticfiles'
 MEDIA_ROOT  = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'post/static')
 ),
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 
 # Default primary key field type
